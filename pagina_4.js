ef2bca33685bd100eb749a839011dd7e9828f0a0
ryanssilva7 <ryan.s.silva7@ba.estudante.senai.br>
2026-06-16 14:57:26 -0300

    feito divisão

---
+++ pagina_4.js
@@ -0,0 +1,40 @@
+let numeroAtual = 5;
+
+function gerarTabuada() {
+    const grade = document.getElementById('grade-resultado');
+    grade.innerHTML = ''; 
+
+    for (let i = 1; i <= 10; i++) {
+        let resultado = numeroAtual / i;
+
+        const linha = document.createElement('div');
+        linha.className = 'linha-tabuada';
+        linha.innerHTML = `
+            <span>${numeroAtual} / ${i}</span>
+            <span>= ${resultado}</span>
+        `;
+        
+        grade.appendChild(linha);
+    }
+}
+
+function alterarNumero(valor) {
+    numeroAtual += valor;
+    if (numeroAtual < 1) numeroAtual = 1;
+    document.getElementById('num-display').innerText = numeroAtual;
+    gerarTabuada();
+}
+
+function irParaSoma() {
+    window.location.href = "index.html";
+}
+
+function irParaPagina3() {
+    window.location.href = "pagina_3.html";
+}
+
+function irParaPagina2() {
+    window.location.href = "pagina_2.html";
+}
+
+window.onload = gerarTabuada;
